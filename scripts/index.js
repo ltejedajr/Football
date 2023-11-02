0d1106acb66dc93f86f397402feec4ab5cae61a0
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-11-01 19:41:37 -0500

    Completed index for Football exercise pg 3-7

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,25 @@
 "use strict";
 
-let teams = [
-    {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
-    {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
-    {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
-    {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},
-];
+window.onload = function () {
+  initDropdown();
+}
 
-window.onload = function() {
-    let states = [];
-    let abbrev = [];
+function initTeamsDropDown() {
 
-    
+  let teams = [
+    { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
+    { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
+    { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
+    { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
+  ];
+
+  const footballTeamList = document.getElementById("footballTeamList");
+
+  let numTeams = teams.length;
+  for (let i = 0; i < numTeams; i++) {
+
+    let options = new Option(numTeams[i].name, numTeams[i].code)
+
+    footballTeamList.appendChild(options);
+  }
 }
